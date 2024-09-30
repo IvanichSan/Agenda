429d2b57702bcab4d55601cb955d3d105acfe091
IvanichSan <santiagol19@outlook.com>
2024-09-29 22:19:37 -0500

    Creacion de la Agenda

---
+++ script.js
@@ -0,0 +1,27 @@
+const eventForm = document.getElementById('event-form');
+const eventTitleInput = document.getElementById('event-title');
+const eventFechaInput = document.getElementById('event-fecha');
+const eventList = document.getElementById('events');
+
+eventForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const eventTitle = eventTitleInput.value;
+    const eventFecha = eventFechaInput.value;
+
+    if (eventTitle===''|| eventFecha==='') {
+        alert('Por Favor completa los campos!!');
+        return;
+    }
+
+    const li = document.createElement('li');
+    li.innerHTML = `<span>${eventTitle} - ${eventFecha}</span> <button class="delete-btn">Eliminar Evento</button>`
+
+    eventList.appendChild(li)
+
+    eventForm.reset();
+    
+    li.querySelector('.delete-btn').addEventListener('click', function(){
+        li.remove();
+    })
+});
